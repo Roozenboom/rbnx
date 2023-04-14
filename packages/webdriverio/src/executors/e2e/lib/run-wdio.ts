03789b7887bb4b7e1dcfb15bf34f35ec955224f6
Richard Roozenboom <richard.roozenboom@outlook.com>
2023-04-14 15:25:04 +0200

    feat: add option to enable wdio watch mode

--- packages/webdriverio/src/executors/e2e/lib/run-wdio.ts
+++ packages/webdriverio/src/executors/e2e/lib/run-wdio.ts
@@ -12,16 +12,22 @@ import { unlink } from 'node:fs/promises';
 import type { NormalizedSchema } from '../schema';
 
 export async function runWdio(options: NormalizedSchema) {
-  const { projectRoot, configFile, spec, suite } = options;
+  const { projectRoot, configFile, spec, suite, watch } = options;
 
   let command = `${getPackageManagerCommand().exec} wdio ${configFile}`;
   if (spec) command += ` --spec=${spec}`;
   if (suite) command += ` --suite=${suite}`;
+  if (watch) command += ` --watch`;
 
   await new Promise((resolve, reject) => {
     exec(command, { cwd: projectRoot }, (error, stdout, stderr) => {
       error ? reject(error) : resolve({ stdout, stderr });
     }).stdout.pipe(process.stdout);
+
+    if (watch) {
+      process.on('SIGINT', () => resolve({}));
+      process.on('SIGTERM', () => resolve({}));
+    }
   });
 }
 
